c0a44bb9d3bbb21b76b334fb8b9425c222e0e6f6
benjamin-BEFOLE <befolebenjamin@yahoo.fr>
2019-12-11 16:35:25 +0100

    add: reconnaissance instructions et boucles

--- level_0/ast.c
+++ level_0/ast.c
@@ -4,72 +4,57 @@
 #include "ast.h"
 
 ast* ast_new_operation(ast_type type, ast* left, ast* right) {
-  ast* new = malloc(sizeof(ast));
-  new->type = type;
-  new->left = left;
-  new->right = right;
-  return new;
+    ast* new = malloc(sizeof(ast));
+    new->type = type;
+    new->left = left;
+    new->right = right;
+    return new;
 }
 
 ast* ast_new_number(int number) {
-  ast* new = malloc(sizeof(ast));
-  new->type = AST_NUMBER;
-  new->number = number;
-  return new;
+    ast* new = malloc(sizeof(ast));
+    new->type = AST_NUMBER;
+    new->number = number;
+    return new;
 }
 
 ast* ast_new_id(char* id) {
-  ast* new = malloc(sizeof(ast));
-  new->type = AST_ID;
-  new->id = strdup(id);
-  return new;
+    ast* new = malloc(sizeof(ast));
+    new->type = AST_ID;
+    new->id = id;
+    return new;
 }
 
 void ast_free(ast* ast) {
-    switch (ast->type) {
-        case AST_NUMBER:
-            break;
-        case AST_ID:
-            free(ast->id);
-            break;
-        case AST_ADD:
-            ast_free(ast->left);
-            ast_free(ast->right);
-            break;
-        case AST_AFFECT:
-            ast_free(ast->left);
-            ast_free(ast->right);
-            break;
-        case AST_SUB:
-            ast_free(ast->left);
-            ast_free(ast->right);
-            break;
-        case AST_MUL:
-            ast_free(ast->left);
-            ast_free(ast->right);
-            break;
-        case AST_DIV:
-            ast_free(ast->left);
-            ast_free(ast->right);
-            break;
+    if (ast != NULL) {
+        switch (ast->type) {
+            case AST_NUMBER:
+                break;
+            case AST_ID:
+                free(ast->id);
+                break;
+            default:
+                ast_free(ast->left);
+                ast_free(ast->right);
+                break;
+        }
     }
     free(ast);
 }
 
 void ast_print(ast* ast, int indent) {
+    if (ast == NULL)
+        return ;
+
     for (int i = 0; i < indent; i++)
         printf("    ");
+
     switch(ast->type) {
         case AST_ID:
             printf("ID (%s)\n", ast->id);
             break;
         case AST_NUMBER:
             printf("NUMBER (%d)\n", ast->number);
-            break;
-        case AST_AFFECT:
-            printf("=\n");
-            ast_print(ast->left, indent + 1);
-            ast_print(ast->right, indent + 1);
             break;
         case AST_ADD:
             printf("+\n");
@@ -90,6 +75,45 @@ void ast_print(ast* ast, int indent) {
             printf("/\n");
             ast_print(ast->left, indent + 1);
             ast_print(ast->right, indent + 1);
+            break;
+        case AST_AFFECT:
+            printf("=\n");
+            ast_print(ast->left, indent + 1);
+            ast_print(ast->right, indent + 1);
+            break;
+        case AST_FOR:
+            printf("for\n");
+            ast_print(ast->left, indent + 1);
+            ast_print(ast->right, indent + 1);
+            break;
+        case AST_WHILE:
+            printf("while\n");
+            ast_print(ast->left, indent + 1);
+            ast_print(ast->right, indent + 1);
+            break;
+        case AST_IF:
+            printf("if\n");
+            ast_print(ast->left, indent + 1);
+            ast_print(ast->right, indent + 1);
+            break;
+        case AST_ELSE:
+            printf("else\n");
+            ast_print(ast->left, indent + 1);
+            ast_print(ast->right, indent + 1);
+            break;
+        case AST_ELSE_IF:
+            printf("else if\n");
+            ast_print(ast->left, indent + 1);
+            ast_print(ast->right, indent + 1);
+            break;
+        case AST_LIST:
+            printf("list\n");
+            ast_print(ast->left, indent + 1);
+            ast_print(ast->right, indent);
+            break;
+        case AST_BLOCK:
+            printf("block\n");
+            ast_print(ast->left, indent + 1);
             break;
     };
 }
--- level_0/compilateur.y
+++ level_0/compilateur.y
@@ -18,8 +18,13 @@
 
 %token <string> ID
 %token <value> NUMBER
+%type <ast> block
+%type <ast> instructions_list
+%type <ast> instruction
 %type <ast> affectation
 %type <ast> arith_expr
+
+%token FOR WHILE IF ELSE 
 
 %left '+' '-'
 %left '*' '/'
@@ -27,21 +32,40 @@
 %%
 
 axiom:
-    affectation '\n'            { parser_ast = $1; return 0; }
+    block                       { parser_ast = $1; return 0; }
+    ;
+
+block:
+    '{' '}'                         { $$ = ast_new_operation(AST_BLOCK, NULL, NULL); }
+    | '{' instructions_list '}'     { $$ = ast_new_operation(AST_BLOCK, $2, NULL); }
+    ;
+
+instructions_list:
+    instruction                         { $$ = ast_new_operation(AST_LIST, $1, NULL); }
+    | instruction instructions_list     { $$ = ast_new_operation(AST_LIST, $1, $2); }
+    ;
+
+instruction:
+    affectation ';'                 { $$ = $1; }
+    | FOR block                     { $$ = ast_new_operation(AST_FOR, NULL, $2); }
+    | WHILE block                   { $$ = ast_new_operation(AST_WHILE, NULL, $2); }
+    | IF block                      { $$ = ast_new_operation(AST_IF, NULL, $2); }
+    | ELSE block                    { $$ = ast_new_operation(AST_ELSE, NULL, $2); }
+    | ELSE IF block                 { $$ = ast_new_operation(AST_ELSE_IF, NULL, $3); }
     ;
 
 affectation:
-    ID '=' arith_expr ';'       { $$ = ast_new_operation(AST_AFFECT, ast_new_id($1), $3); }
+    ID '=' arith_expr           { $$ = ast_new_operation(AST_AFFECT, ast_new_id($1), $3); }
     ;
 
 arith_expr:
-    arith_expr '+' arith_expr   { $$ = ast_new_operation(AST_ADD, $1, $3); }
-    | arith_expr '-' arith_expr { $$ = ast_new_operation(AST_SUB, $1, $3); }
-    | arith_expr '*' arith_expr { $$ = ast_new_operation(AST_MUL, $1, $3); }
-    | arith_expr '/' arith_expr { $$ = ast_new_operation(AST_DIV, $1, $3); }
-    | '(' arith_expr ')'        { $$ = $2; }
-    | ID                        { $$ = ast_new_id($1); }
-    | NUMBER                    { $$ = ast_new_number($1); }
+    arith_expr '+' arith_expr       { $$ = ast_new_operation(AST_ADD, $1, $3); }
+    | arith_expr '-' arith_expr     { $$ = ast_new_operation(AST_SUB, $1, $3); }
+    | arith_expr '*' arith_expr     { $$ = ast_new_operation(AST_MUL, $1, $3); }
+    | arith_expr '/' arith_expr     { $$ = ast_new_operation(AST_DIV, $1, $3); }
+    | '(' arith_expr ')'            { $$ = $2; }
+    | ID                            { $$ = ast_new_id($1); }
+    | NUMBER                        { $$ = ast_new_number($1); }
     ;
 
 %%
